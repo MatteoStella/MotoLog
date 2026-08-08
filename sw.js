ce0bcce9f0f6a86c96ae68a5565a1a76f1a9143c
MotoLog Deploy <motolog@local>
2026-08-08 14:44:54 +0000

    Deploy MotoLog come PWA statica (index, manifest, service worker, icone)

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE = 'motolog-v1';
+const SHELL = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+// Network-first per restare aggiornati, fallback alla cache se offline.
+self.addEventListener('fetch', (e) => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request)
+      .then((res) => {
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
+
+self.addEventListener('periodicsync', (e) => {
+  if (e.tag === 'km-reminder') {
+    e.waitUntil(self.registration.showNotification('MotoLog', {
+      body: 'Ricordati di aggiornare i km dei tuoi mezzi',
+      icon: './icon-192.png'
+    }));
+  }
+});
