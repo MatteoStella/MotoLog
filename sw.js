2e735a9405f8544a0ca2dab9deacd08a108e10b0
MyGarage Deploy <mygarage@local>
2026-09-24 05:10:03 +0000

    Aggiorno versione cache SW

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'motolog-v4';
+const CACHE = 'motolog-v5';
 const SHELL = ['./', './index.html', './app.js', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
